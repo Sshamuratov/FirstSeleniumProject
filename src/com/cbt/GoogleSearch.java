8717d3b8a2746bbc43c748ff23f0c9237e1785d0
Sshamuratov <bzikbzik775@gmail.com>
2018-06-09 17:19:30 -0400

    wooden spoon search automation

---
+++ src/com/cbt/GoogleSearch.java
@@ -0,0 +1,17 @@
+package com.cbt;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleSearch {
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\bzikb\\Documents\\selenium dependencies\\drivers\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.etsy.com/search?q=wooden%20spoon");
+	}
+
+}
